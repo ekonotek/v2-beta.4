4f39c271efa654861001a2f620ea4c51f360d457
Rafael <ekonotek@gmail.com>
2022-10-04 09:35:34 -0500

    finish tutorial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,5 @@
+project: path.join(__dirname, "tsconfig.eslint.json")
 module.exports = require("@blitzjs/next/eslint")
+
+
+// https://github.com/microsoft/vscode-eslint/issues/1170
---
+++ app/questions/mutations/createQuestion.ts
@@ -0,0 +1,19 @@
+import { resolver } from "@blitzjs/rpc";
+import db from "db";
+import {CreateQuestion} from "../validations"
+
+export default resolver.pipe(
+  resolver.zod(CreateQuestion),
+  resolver.authorize(),
+  async (input) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const question = await db.question.create({ data: {
+      text: input.text,
+      Choice: {
+        create: input.Choice
+      },
+    } });
+
+    return question;
+  }
+);
